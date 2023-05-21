7f8ef8f29d4140c89f34d84e3cae470ded5ec76a
KES123450 <a55208806@gmail.com>
2023-05-21 21:05:47 +0900

    소셜매칭기능 추가

---
+++ app/src/main/java/com/example/matchingproto/MatchSuccessActivity.kt
@@ -0,0 +1,160 @@
+package com.example.matchingproto
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.matchingproto.databinding.ActivityMainBinding
+import com.example.matchingproto.databinding.MatchSuccessBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MatchSuccessActivity: AppCompatActivity() , OnMapReadyCallback {
+    var myID = "김은서" // 임시로 설정해둔 내ID
+    lateinit var mateID:String
+    lateinit var matchBinding: MatchSuccessBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    var mylatitude:Double =0.0
+    var mylongitude:Double=0.0
+
+    var matelatitude:Double =0.0
+    var matelongitude:Double=0.0
+
+    // 파베에서 데이터 불러올 주기(ms) 설정
+    private val interval = 1000
+
+    // Handler 객체 생성
+    private val handler = Handler()
+
+    var googleMap: GoogleMap? = null
+
+    val userLocDB: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        matchBinding=MatchSuccessBinding.inflate(layoutInflater)
+        mateID = intent.getStringExtra("mateName").toString()
+        myID= intent.getStringExtra("myID").toString()
+        matchBinding.nickname.text=mateID
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(10 * 1000) // 10 seconds
+            .setFastestInterval(1 * 1000) // 1 second
+        setContentView(matchBinding.root)
+        (supportFragmentManager.findFragmentById(R.id.mapView) as
+                SupportMapFragment?)!!.getMapAsync(this)
+        getMateLoc()
+        val runnable = object : Runnable {
+            override fun run() {
+                Log.d("log",mylatitude.toString())
+
+                setUserLoc()
+
+                googleMap?.clear()
+                makeMarker(mylatitude,mylongitude,myID)
+                makeMarker(matelatitude,matelongitude,mateID)
+
+                // 일정 시간 간격으로 다시 실행
+                handler.postDelayed(this, interval.toLong())
+            }
+        }
+        handler.postDelayed(runnable, interval.toLong())
+
+    }
+
+    // 상대방의 위치정보를 받아서 내 앱에 업데이트
+    private fun getMateLoc(){
+        userLocDB.collection("User_Loc")
+            .document(mateID)
+            .addSnapshotListener { documentSnapshot, e ->
+                if (e != null) {
+                    Log.d("Firestore", "Listen failed: $e")
+                    return@addSnapshotListener
+                }
+
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    val documentData = documentSnapshot.data
+                    matelatitude = documentData?.get("latitude") as Double
+                    matelongitude = documentData?.get("longitude") as Double
+
+                } else {
+                    Log.d("Firestore", "Document does not exist")
+                }
+            }
+    }
+
+
+
+        //내 위치정보를 fetchLocation으로 받아서 파이어베이스에 업데이트
+    private fun setUserLoc(){
+        fetchLocation()
+
+        userLocDB.collection("User_Loc")
+            .document(myID)
+            .update(mapOf(
+                "latitude" to mylatitude,
+                "longitude" to mylongitude
+            ))
+
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult?.lastLocation?.let {
+                    mylatitude = it.latitude
+                    mylongitude = it.longitude
+                    // do something with latitude and longitude
+                }
+            }
+        }, null)
+    }
+
+    private fun makeMarker(latitude:Double,longitude:Double,ID:String){
+        //마커표시
+
+        val markerOption= MarkerOptions()
+        markerOption.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker2))
+        markerOption.position(LatLng(latitude, longitude))
+        markerOption.title(ID)
+
+        googleMap?.addMarker(markerOption)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        googleMap = p0
+
+    }
+}
---
+++ app/src/main/java/com/example/matchingproto/WaitParticipateActivity.kt
@@ -0,0 +1,90 @@
+package com.example.matchingproto
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.matchingproto.databinding.WaitParticipantsBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.firebase.firestore.FirebaseFirestore
+
+class WaitParticipateActivity : AppCompatActivity(), OnMapReadyCallback {
+    lateinit var binding: WaitParticipantsBinding
+    lateinit var partyID:String
+    val partyDB: FirebaseFirestore = FirebaseFirestore.getInstance()
+    var myID:String="test"
+    var googleMap: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        partyID = intent.getStringExtra("partyID").toString()
+        myID=intent.getStringExtra("myID").toString()
+        binding=WaitParticipantsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        waitParticipant()
+        (supportFragmentManager.findFragmentById(R.id.mapView) as
+                SupportMapFragment?)!!.getMapAsync(this)
+    }
+
+    private fun waitParticipant(){
+        partyDB.collection("party")
+            .document(partyID)
+            .addSnapshotListener { documentSnapshot, e ->
+                if (e != null) {
+                    Log.d("Firestore", "Listen failed: $e")
+                    return@addSnapshotListener
+                }
+
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    val documentData = documentSnapshot.data
+
+
+                    val participateCheck = documentData?.get("participate_check") as? Boolean
+                    if (participateCheck != null) {
+                        // participateCheck 사용하는 코드
+                        // ...
+                    } else {
+                        // participateCheck가 null인 경우 처리
+                        // ...
+                    }
+
+
+                    if(participateCheck == true){
+                        val participantID = documentData?.get("participantID") as? String
+                        if (participantID != null) {
+                            // participantID 사용하는 코드
+                            // ...
+                        } else {
+                            // participantID가 null인 경우 처리
+                            // ...
+                        }
+
+                        partyDB.collection("User_Loc")
+                            .document(myID)
+                            .set(
+                                mapOf(
+                                    "longitude" to 0.0,
+                                    "latitude" to 0.0
+                                )
+                            )
+                        val intent: Intent = Intent(this,MatchSuccessActivity::class.java)
+                        intent.putExtra("mateName",participantID)
+                        intent.putExtra("myID",myID)
+                        startActivity(intent)
+
+                    }
+
+
+                } else {
+                    Log.d("Firestore", "Document does not exist")
+                }
+            }
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        googleMap = p0
+
+    }
+}
